4b036e8a18e2cb6f1bd1bfd4fbf9b8f8ee5fdf31
JaMi-01 <117598293+JaMi-01@users.noreply.github.com>
2025-10-14 20:44:39 +0200

    Update page.jsx

--- app/searchResults/page.jsx
+++ app/searchResults/page.jsx
@@ -1,3 +1,6 @@
+<Suspense fallback={<Loader />}>
+  <SearchResults />
+</Suspense>
 'use client';
 import React, { useEffect, useState } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
