d24a8c9c1674537e043c07eb98bd930290aae267
JaMi-01 <117598293+JaMi-01@users.noreply.github.com>
2025-10-14 13:42:05 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { appDir: true }
+  swcMinify: true,
+  images: {
+    domains: ['cdn.nomstead.com', 'api.nomstead.com', 'nomstead.com']
+  },
+  eslint: { ignoreDuringBuilds: true }
 };
+
 module.exports = nextConfig;
