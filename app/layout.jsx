7013b5e20a74eadb1e86068cc8e20473f9b93769
JaMi-01 <117598293+JaMi-01@users.noreply.github.com>
2025-10-15 08:49:39 +0200

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+import './styles/globals.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+export const metadata = {
+  title: 'Nomstead Open Marketplace',
+  description: 'Buy/Sell marketplace for Nomstead - Open Marketplace'
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="da">
+      <body>
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          <main className="flex-1 container">
+            {children}
+          </main>
+          <Footer />
+        </div>
+      </body>
+    </html>
+  )
+}
