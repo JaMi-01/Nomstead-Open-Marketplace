531ee614c97c9250fe12230d82b5649867c0a0fc
JaMi-01 <117598293+JaMi-01@users.noreply.github.com>
2025-10-15 19:01:20 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['cdn.nomstead.com','api.nomstead.com','nomstead.com']
+  },
+  eslint: { ignoreDuringBuilds: true }
+};
+
+module.exports = nextConfig;
